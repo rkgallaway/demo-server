fe1b106dc6c62f3b96a398a2b2d2b285ad0bdfa8
Ryan Gallaway <rkgallaway@msn.com>
2024-02-08 15:03:16 -0800

    proof of life

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const req = require('express/lib/request');
+
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3001
+
+app.get('/', (req, res, next) => {
+
+    const serverIpAddress = req.socket.localAddress;
+
+    res.status(200).send(`Hello World!  my IP address is ${serverIpAddress}`);
+});
+
+app.listen(PORT, () => console.log('Listening on port: ', PORT));

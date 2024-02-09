54a9c7483441079a03a12114a8a0b549235c4817
Ryan Gallaway <rkgallaway@msn.com>
2024-02-08 16:11:10 -0800

    base return json

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ const PORT = process.env.PORT || 8080
 app.get('/', (req, res, next) => {
 
     const serverIpAddress = req.socket.localAddress;
+    const data = {
+        Message: 'Hello World',
+        Region: 'us-west-2',
+        IPv6Address: serverIpAddress
+    }
 
-    res.status(200).send(`Hello World!  my IP address is ${serverIpAddress}`);
+    res.status(200).send(data);
 });
 
 app.listen(PORT, () => console.log('Listening on port: ', PORT));
